911c5fae7224083eaa66128696caafae4cc28160
MB <mbilal_ce@live.com>
2020-11-05 03:38:26 +0000

    Updated sample livy job with appropriate values for event logging

--- examples/livy-example/example.py
+++ examples/livy-example/example.py
@@ -33,7 +33,13 @@ batch = LivyBatch.create(
     class_name="org.apache.spark.examples.SparkPi",
     verify=False,
     spark_conf= {
-                "spark.kubernetes.namespace": "livy"
+                "spark.kubernetes.namespace": "spark-cluster",
+                "spark.kubernetes.driver.volumes.persistentVolumeClaim.pvc-ddc9eeec-db5f-4134-bb01-becd180ac671.mount.path": "/data",
+                "spark.kubernetes.driver.volumes.persistentVolumeClaim.pvc-ddc9eeec-db5f-4134-bb01-becd180ac671.mount.readOnly": False,
+                "spark.kubernetes.driver.volumes.persistentVolumeClaim.pvc-ddc9eeec-db5f-4134-bb01-becd180ac671.options.claimName":"events-dir"
+                "spark.kubernetes.executor.volumes.persistentVolumeClaim.pvc-ddc9eeec-db5f-4134-bb01-becd180ac671.mount.path": "/data",
+                "spark.kubernetes.executor.volumes.persistentVolumeClaim.pvc-ddc9eeec-db5f-4134-bb01-becd180ac671.mount.readOnly": False,
+                "spark.kubernetes.executor.volumes.persistentVolumeClaim.pvc-ddc9eeec-db5f-4134-bb01-becd180ac671.options.claimName":"events-dir"
             },
 )
 # batch.wait()
